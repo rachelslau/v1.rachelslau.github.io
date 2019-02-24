be251723aa96251fc6de9d25853f9aeee897160f
Rachel Lau <rachellau@berkeley.edu>
2019-02-24 13:37:00 -0800

    some coffee changes

--- src/Coffee.js
+++ src/Coffee.js
@@ -14,7 +14,7 @@ class Coffee extends Component {
           <div className="title">coffee</div>
           <div className="coffee-shadow"></div>
           <br /><br />
-          Before coming to college, I never thought I would be a coffee person. I couldn’t even walk into Starbucks without getting nausea from the smell of coffee. But now that I’m in college and actually need a caffeine kick every once in a while, I decided to make a blog where I review drinks from some of the coffee shops I’ve visited during early mornings and late nights. Check it out&nbsp;
+          Before coming to college, I never thought I would be a coffee person. I couldn’t even walk into Starbucks without getting dizzy from the smell of coffee. But now that I’m pulling long nights and actually need a caffeine kick every once in a while, I decided to make a blog where I review drinks from some of the coffee shops I’ve visited during early mornings and late nights. Check it out&nbsp;
           <a
             className="coffee-link-animation"
             href="https://anticoffeecoffeeclub.tumblr.com/"
